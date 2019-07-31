bd0adc9593f845bb155a733b292b2b528424e22a
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-07-31 11:26:14 -0500

    Add sub function

--- nn/utils/MatrixFunctions.js
+++ nn/utils/MatrixFunctions.js
@@ -12,23 +12,34 @@ export default class MatrixFunctions {
 				sum = 0;
 				for (let k = 0; k < colA; k++) {
 					sum += matrixA[IX(j, k, colA)] * matrixB[IX(k, i, colB)];
-        }
-        temp.push(sum);
+				}
+				temp.push(sum);
 			}
-    }
-    return temp, temp_shape;
-  }
-  transpose(matrix, row, col) {
-    temp = [];
-    temp_shape = [row, col];
-    for (let i = 0; i < col; i++){
-      n = 1;
-      for (let j = 0; j < row; j++){
-        temp.push(matrix[n + i]);
-        n += col;
-      }
-    }
-    return temp, temp_shape;
+		}
+		return temp, temp_shape;
+	}
+	transpose (matrix, row, col) {
+		temp = [];
+		temp_shape = [ row, col ];
+		for (let i = 0; i < col; i++) {
+			n = 0;
+			for (let j = 0; j < row; j++) {
+				temp.push(matrix[n + i]);
+				n += col;
+			}
+		}
+		return temp, temp_shape;
+	}
+	sub (matrixA, rowA, colA, matrixB) {
+		if (matrixA.length != matrixB.length) {
+			console.log('Sub Function error: Dimensions are not the same');
+		}
+		temp = [];
+		temp_shape = [ rowA, colA ];
+		for (let i = 0; i < matrixA.length; i++) {
+			temp.push(matrixA[i] - matrixB[i]);
+		}
+		return temp, temp_shape;
   }
   
 }
