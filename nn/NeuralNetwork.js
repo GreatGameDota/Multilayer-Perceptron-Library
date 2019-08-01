406a0c0e497ca3b15083522391313d439b725e4e
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-07-31 21:26:06 -0500

    Add computCost function

--- nn/NeuralNetwork.js
+++ nn/NeuralNetwork.js
@@ -1,3 +1,5 @@
+import mf from './utils/MatrixFunctions';
+
 class NeuralNetwork {
 	constructor (lr) {
 		learningRate = lr;
@@ -11,6 +13,16 @@ class NeuralNetwork {
 		prevWeightShape = this.linearLayers.length > 0 ? this.linearLayers[this.linearLayers.length - 1].W_shape : null;
 		this.linearLayers.push(new LinearLayer(units, inputShape, weight_init, prevWeightShape));
 		this.activationLayers.push(new ActivationLayer());
+	}
+	computCost (Y, rowY, colY, Y_hat, rowY2, colY2) {
+		m = colY;
+		[ temp, temp_shape ] = mf.sub(Y, rowY, colY, Y_hat, rowY2, colY2);
+		[ temp2, temp2_shape ] = mf.square(temp, temp_shape[0], temp_shape[1]);
+		[ temp3, temp3_shape ] = mf.sum(temp2, temp2_shape[0], temp2_shape[1], 1);
+		this.cost = 1 / (2 * m);
+		this.cost *= temp3;
+		[ temp4, temp4_shape ] = mf.mult(temp, temp_shape[0], temp_shape[1], -1 / m);
+		return [ temp4, temp4_shape ];
 	}
 	train (epochs) {
 		//TODO
